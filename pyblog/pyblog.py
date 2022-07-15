b3156208bb50fca9f62381e8a056c318f2d06909
Albert Cole <Albert.Cole@libertymutual.com>
2022-07-15 15:44:29 -0400

    set up

--- pyblog/pyblog.py
+++ pyblog/pyblog.py
@@ -1,72 +1,118 @@
+from textwrap import indent
 import requests
 import json
 import sys
 import os
 import argparse
+import re
+from datetime import datetime
+from requests.auth import HTTPBasicAuth
 
+#Read In Env Variables
+
+#wp_user = os.environ['wp_admin']
+#wp_pass = os.environ['wp_pass']
+#wp_url = os.environ['wp_url']
+
+#Hard coded values
+#Remove these when env variables are set above
+wp_user = "tmpstag"
+wp_pass = "wjI1 EQFx MOsT H8wH uVkc IeJY"
+wp_url = "http://54.90.224.76:8088/wp-json/wp/v2/posts"
+
+#Get current date and time for new posts
+current_date = datetime.now()
+blog_post_date = current_date.strftime('%Y-%m-%dT%H:%M:%M')
+
+#Create authentication portion of request
+basic = HTTPBasicAuth(wp_user, wp_pass)
+
+#Wordpress API header
+headers = {
+  'Accept': 'application/json',
+  }
+
+#Use argparse to take in variables
 my_parser = argparse.ArgumentParser(prog='pyblog', description='Program used to read and make blog posts')
 my_parser.add_argument('command', type=str)
 my_parser.add_argument('-f', '--file', help="Name of the file to create blog post from")
 my_parser.add_argument('text_input', nargs='?', type=str, default="-", help="Name of the file to create blog post from")
 
+#Set args to variables taken in by argparse
 args = my_parser.parse_args()
 
-default_url = "http://127.0.0.1/blog/index.php/wp-json/wp/v2/posts"
-
-headers = {
-  'Accept': 'application/json',
-  #'Content-Type': 'application/json',
-  'Authorization': 'Basic d29yZHByZXNzLWFkbWluOjhCVG4gWDRrQyBxbklYIHNueVIgQmxwWiBpSzVN'
-}
-
-#filename = "latinfile"
 def post():
 
+    title = "Need to define how to get this from stdin"
+    excerpt = "Same as title"
+
     rest_method = "POST"
-    url = default_url
+    url = wp_url
     payload={'Content': 'Test Content',
-    'title': 'Python Test Post',
-    'excerpt': 'Posted using Python',
-    'status': 'publish'}
+    'title': title,
+    'excerpt': excerpt,
+    'date': blog_post_date,
+    'status': 'publish'
+    }
 
-    response = requests.request(rest_method, url, headers=headers, data=payload)
+    response = requests.request(rest_method, url, auth=basic, headers=headers, data=payload)
 
-    print(response.text)
+    pyblog_request_data = response.json()
+
+    #print(response.text)
+    print(json.dumps(pyblog_request_data, indent=4, sort_keys=True) )
 
 def upload():
     input_file = args.file or args.text_input
-    for input_file in sys.stdin:
-       
-        lines = input_file
-        title = lines.split('\n', 1)[0]              
-        excerpt = lines.split('\n', 2)[2]
-
+    if input_file == "-":
+        file = sys.stdin
     else:
-        with open(input_file) as file:
-            lines = file.read()
-            title = lines.split('\n', 1)[0]
-            excerpt = lines.split('\n', 2)[2]
+        file = open(input_file)  
+       
+    lines = file.read()
+    title = lines.split('\n', 1)[0]
+    excerpt = lines.split('\n', 2)[2]
 
+    file.close()
    
     rest_method = "POST"
-    url = default_url
+    url = wp_url
     payload={'Content': 'File Content',
     'title': title,
     'excerpt': excerpt,
-    'status': 'publish'}
+    'date': blog_post_date,
+    'status': 'publish',
+    }
 
-    response = requests.request(rest_method, url, headers=headers, data=payload)
+    response = requests.request(rest_method, url, auth=basic, headers=headers, data=payload)
 
-    print(response.text)
+    pyblog_request_data = response.json()
 
+    pyblog_request_data = response.json()
+
+    date_response = pyblog_request_data["date"]
+    title_response = pyblog_request_data["title"]["rendered"]
+    excerpt_response = pyblog_request_data["excerpt"]["rendered"]
+    excerpt_response = re.sub(r'<.*?>', '', excerpt_response)
+
+    print(f' Date: {date_response}  \n\n Title: {title_response} \n\n Excerpt: {excerpt_response}')
 def read():
 
     rest_method = "GET"
-    url = default_url +"?per_page=1"
+    url = wp_url +"?per_page=1"
    
     response = requests.request(rest_method, url, headers=headers)
+   
+    pyblog_request_data = response.json()
 
-    print(response.text)
+    title_response = pyblog_request_data[0]["title"]["rendered"]
+    excerpt_response = pyblog_request_data[0]["excerpt"]["rendered"]
+    excerpt_response = re.sub(r'<.*?>', '', excerpt_response)
+   
+    #print(response.text)
+    print(f' Title: {title_response} \n\n Excerpt: {excerpt_response}')
+
+   
 
 if args.command == "read":
     read()
