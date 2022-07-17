ced8057ef6977af945973addd3e6c449414e9649
Albert Cole <Albert.Cole@libertymutual.com>
2022-07-17 19:26:57 -0400

    edits for flake8

--- pyblog/pyblog.py
+++ pyblog/pyblog.py
@@ -9,38 +9,20 @@ from datetime import datetime
 from requests.auth import HTTPBasicAuth
 
 #Read In Env Variables
-
-#wp_user = os.environ['wp_admin']
-#wp_pass = os.environ['wp_pass']
-#wp_url = os.environ['wp_url']
-
-#Hard coded values
-#Remove these when env variables are set above
-wp_user = "TMPPROD"
-wp_pass = "qNHJ FYVs NL9g Vakc AN14 na6Z"
-wp_url = "http://54.164.17.201:8088/wp-json/wp/v2/posts"
-
-#Get current date and time for new posts
+wp_user = os.environ['wp_admin']
+wp_pass = os.environ['wp_pass']
+wp_url = os.environ['wp_url']
 current_date = datetime.now()
 blog_post_date = current_date.strftime('%Y-%m-%dT%H:%M:%M')
-
-#Create authentication portion of request
 basic = HTTPBasicAuth(wp_user, wp_pass)
-
-#Wordpress API header
 headers = {
   'Accept': 'application/json',
   }
-
-#Use argparse to take in variables
 my_parser = argparse.ArgumentParser(prog='pyblog', description='Program used to read and make blog posts')
 my_parser.add_argument('command', type=str)
 my_parser.add_argument('-f', '--file', help="Name of the file to create blog post from")
 my_parser.add_argument('text_input', nargs='?', type=str, default="-", help="Name of the file to create blog post from")
-
-#Set args to variables taken in by argparse
 args = my_parser.parse_args()
-
 def post():
 
     title = "Need to define how to get this from stdin"
@@ -59,9 +41,7 @@ def post():
 
     pyblog_request_data = response.json()
 
-    #print(response.text)
     print(json.dumps(pyblog_request_data, indent=4, sort_keys=True) )
-
 def upload():
     input_file = args.file or args.text_input
     if input_file == "-":
@@ -72,9 +52,7 @@ def upload():
     lines = file.read()
     title = lines.split('\n', 1)[0]
     excerpt = lines.split('\n', 2)[2]
-
     file.close()
-   
     rest_method = "POST"
     url = wp_url
     payload={'Content': 'File Content',
@@ -83,39 +61,25 @@ def upload():
     'date': blog_post_date,
     'status': 'publish',
     }
-
     response = requests.request(rest_method, url, auth=basic, headers=headers, data=payload)
-
     pyblog_request_data = response.json()
-
     pyblog_request_data = response.json()
-
     date_response = pyblog_request_data["date"]
     title_response = pyblog_request_data["title"]["rendered"]
     excerpt_response = pyblog_request_data["excerpt"]["rendered"]
     excerpt_response = re.sub(r'<.*?>', '', excerpt_response)
-
     print(f' Date: {date_response}  \n\n Title: {title_response} \n\n Excerpt: {excerpt_response}')
 def read():
-
     rest_method = "GET"
     url = wp_url +"?per_page=1"
-   
     response = requests.request(rest_method, url, headers=headers)
-   
     pyblog_request_data = response.json()
-
     title_response = pyblog_request_data[0]["title"]["rendered"]
     excerpt_response = pyblog_request_data[0]["excerpt"]["rendered"]
     excerpt_response = re.sub(r'<.*?>', '', excerpt_response)
-   
-    #print(response.text)
     print(f' Title: {title_response} \n\n Excerpt: {excerpt_response}')
-
-   
 
 if args.command == "read":
     read()
-
 if args.command == "upload":
     upload()
