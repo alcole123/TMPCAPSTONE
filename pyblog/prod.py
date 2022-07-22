9cbbf05d0a05bebd8072ca00c7840cdbf8dd4e93
Albert Cole <Albert.Cole@libertymutual.com>
2022-07-22 11:22:53 -0400

    update

--- pyblog/prod.py
+++ pyblog/prod.py
@@ -4,9 +4,25 @@ import requests
 
 def prod_test():
     r=requests.get("http://54.164.17.201:8088/wp-json/wp/v2/posts")
-    return r.status_code
+    s=r.status_code
+    return s
+
+
+
+def prod_test_2():
+    r=requests.get("http://54.164.17.201:8088/wp-json/wp/v2/posts")
+    js=r.json()
+    t=" "
+    for i,data in enumerate(js):
+        t=js[i]['id']
+        return t
+
+
+t=prod_test_2()
+print(t)
 
 def test_answer():
     assert prod_test() == 200
 
-
+def test_answer2():
+    assert prod_test_2() == 13
